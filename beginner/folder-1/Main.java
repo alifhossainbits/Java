aa03c07622b5d03301f3f066802852eb5f4ac6ed
alifhossainbits <alifhossain5577@gmail.com>
2024-05-24 10:39:09 +0600

    .

---
+++ beginner/folder-1/Main.java
@@ -0,0 +1,34 @@
+// public class Main {
+//     public static void main(String[] args) {
+//
+
+//         // Example of a simple test
+//         int result = add(2, 3);
+//         if (result == 5) {
+//             System.out.println("Test Passed");
+//         } else {
+//             System.out.println("Test Failed");
+//         }
+//     }
+
+//     public static int add(int a, int b) {
+//         return a + b;
+//     }
+// }
+
+
+public class Main {
+    public static void main(String[] args){
+
+        // Swap Two Variable
+
+
+
+        // System.out.println("Hello world");
+        // int x = 300;
+        // int y = 400;
+        // int total = x + y;
+        // System.out.println(total);
+
+    }
+}
