92f92f433ac256fb19400a0e54154132aedc31f1
alifhossainbits <alifhossain5577@gmail.com>
2024-05-26 09:48:52 +0600

    .

--- beginner/folder-1/Main.java
+++ beginner/folder-1/Main.java
@@ -1,27 +1,22 @@
-// public class Main {
-//     public static void main(String[] args) {
-//
-
-//         // Example of a simple test
-//         int result = add(2, 3);
-//         if (result == 5) {
-//             System.out.println("Test Passed");
-//         } else {
-//             System.out.println("Test Failed");
-//         }
-//     }
-
-//     public static int add(int a, int b) {
-//         return a + b;
-//     }
-// }
-
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
 
 
         ///// User Input ///
+        Scanner doollaaloo = new Scanner(System.in);
+
+        System.out.println("your name? ");
+        String name = doollaaloo.nextLine();
+        System.out.println("Whats your age? ");
+        int age = doollaaloo.nextInt();
+        System.out.println("What's your favorite food?");
+        doollaaloo.nextLine();
+        String food = doollaaloo.nextLine(); // it's skipped at first because after nextInt() it left a extra new line /n (enter) so when it came for food's turn it skipped. to fix it we can add doollaaloo.nextLine() before food.
+
+        System.out.println("Hello Mr."+name+". You're "+age+" years old. and your favorite food is "+food);
+
 
 
         //////// Swap Two Variable /////
