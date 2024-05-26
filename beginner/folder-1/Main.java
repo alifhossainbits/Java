756e55cb5020ef2fcbd00cb6702319ce4099271c
alifhossainbits <alifhossain5577@gmail.com>
2024-05-26 12:37:39 +0600

    CP solved 3 problems

--- beginner/folder-1/Main.java
+++ beginner/folder-1/Main.java
@@ -3,41 +3,23 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
 
+        // Simple Sum
+        Scanner input = new Scanner(System.in);
+        int A = input.nextInt();
+        int B = input.nextInt();
+        int SOMA = A + B;
 
-        ///// User Input ///
-        Scanner doollaaloo = new Scanner(System.in);
+        System.out.println("SOMA = " + SOMA);
+        // System.out.printf("SOMA = %d%n", SOMA);
 
-        System.out.println("your name? ");
-        String name = doollaaloo.nextLine();
-        System.out.println("Whats your age? ");
-        int age = doollaaloo.nextInt();
-        System.out.println("What's your favorite food?");
-        doollaaloo.nextLine();
-        String food = doollaaloo.nextLine(); // it's skipped at first because after nextInt() it left a extra new line /n (enter) so when it came for food's turn it skipped. to fix it we can add doollaaloo.nextLine() before food.
+        // problem 1
+        // Scanner inputR = new Scanner(System.in);
+        // double R = inputR.nextDouble();
+        // double A = Math.PI * Math.pow(R, 2);
 
-        System.out.println("Hello Mr."+name+". You're "+age+" years old. and your favorite food is "+food);
+        // System.out.printf("A=%.4f%n", A);
+        // System.out.printf("A=%.4f\n", A);
 
-
-
-        //////// Swap Two Variable /////
-        // String x = "water";
-        // String y = "red-bull";
-        // String temp;
-
-        // temp = x;
-        // x = y;
-        // y = temp;
-
-        // System.out.println(temp);
-        // System.out.println(x);
-        // System.out.println(y);
-
-
-        // System.out.println("Hello world");
-        // int x = 300;
-        // int y = 400;
-        // int total = x + y;
-        // System.out.println(total);
 
     }
 }
