0982c4bcaa85b82c6227eacd807f0d72b0125f40
alifhossainbits <alifhossain5577@gmail.com>
2024-05-28 21:14:50 +0600

    .

--- beginner/folder-1/Main.java
+++ beginner/folder-1/Main.java
@@ -3,6 +3,30 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
 
+        // 1006 Average 2
+        // Scanner input = new Scanner(System.in);
+        // double A = input.nextDouble();
+        // double B = input.nextDouble();
+        // double C = input.nextDouble();
+
+        // double MEDIA = ((A * 2) + (B * 3) + (C * 5)) / (2 + 3 + 5);
+
+        // System.out.printf("MEDIA = %.1f\n", Media);
+
+
+
+
+        // // 1005 Average 1
+        // Scanner input = new Scanner(System.in);
+        // double A = input.nextDouble();
+        // double B = input.nextDouble();
+        // double MEDIA = ((A * 3.5) + (B * 7.5)) / (3.5 + 7.5);
+
+
+        // System.out.printf("MEDIA = %.5f\n", MEDIA);
+
+
+
         // problem 3: 1004 Simple Product
         // Scanner input = new Scanner(System.in);
         // int A = input.nextInt();
